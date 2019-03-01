ee8a9ede5b62d68b6f71c24cd8ca4dcc0573e36f
allensg891 <32722680+allensg891@users.noreply.github.com>
2019-03-01 10:27:40 -0500

    Update team9.py

--- team9.py
+++ team9.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'YoteBoat9' # Only 10 chars displayed.
+strategy_name = 'BigBrain'
+strategy_description = 'Always betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,9 +26,26 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
-    
+    #strategy 1:
+    #return 'b'
+           
+    #strategy 2:
+    '''if len(my_history)==0 or len(my_history)==1:
+        return 'c'
+    if len(their_history)>=2:
+        if 'bb' in their_history[-2]:
+            return 'b'
+        if 'bc' in their_history[-2]:
+            return 'c'
+        if 'cc' in their_history[-2]:
+            return 'c'
+            '''
+    #strategy 3:
+    if len(my_history)==0:
+        return 'c'
+    if len(my_history)>=1:
+        return their_history[-1]
+        
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
