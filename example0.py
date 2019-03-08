c4a7322fceaf2ffa499883e34890b66be43801b8
Allen Guo <allensg891@gmail.com>
2019-03-08 09:59:33 -0500

    Update Team 9
    Strategy 3: Tit and Tat

--- example0.py
+++ example0.py
@@ -6,21 +6,42 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'E0'
-strategy_name = 'Collude'
-strategy_description = 'Always collude.'
+team_name = 'YoteBoat9' # Only 10 chars displayed.
+strategy_name = 'BigBrain'
+strategy_description = 'Always betray'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
     
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
+    Make my move.
+    Returns 'c' or 'b'. 
     '''
+
+    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    # their_history: a string of the same length as history, possibly empty. 
+    # The first round between these two players is my_history[0] and their_history[0].
+    # The most recent round is my_history[-1] and their_history[-1].
     
-    # This player always colludes.
-    return 'c'
+    # Analyze my_history and their_history and/or my_score and their_score.
+    # Decide whether to return 'c' or 'b'.
     
+    #strategy 1:
+    #return 'b'
+           
+    #strategy 2:
+    '''if len(my_history)==0 or len(my_history)==1:
+        return 'c'
+    if len(their_history)>=2:
+        if 'bb' in their_history[-2]:
+            return 'b'
+        if 'bc' in their_history[-2]:
+            return 'c'
+        if 'cc' in their_history[-2]:
+            return 'c'
+            '''
+    #strategy 3:
+    if len(my_history)==0:
+        return 'c'
+    if len(my_history)>=1:
+        return their_history[-1]
